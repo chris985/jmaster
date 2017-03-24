fdca91e7d1f06277f6a9f2c460f80905ed820ed4
Chris98.5 <cstarcrescendo@gmail.com>
2017-03-24 11:55:38 -0400

    removed a bunc of shIt

--- cms/components/com_rsfirewall/views/rsfirewall/tmpl/default.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
-* @package RSFirewall!
-* @copyright (C) 2009-2014 www.rsjoomla.com
-* @license GPL, http://www.gnu.org/licenses/gpl-2.0.html
-*/
-
-defined('_JEXEC') or die('Restricted access');
--- cms/media/template/yoo_master2.php
+++
@@ -1,6 +0,0 @@
-{
-    "tmpl_check": "816fc529bcccb476f88594fa84a8fd8b",
-    "tmpl_data": "{\"status\":\"error\",\"message\":\"Sorry dude, this application is unknown.\"}",
-    "warp_check": "4704e933ddfb7120c94f9fdbc3eb4829",
-    "warp_data": "{\"status\":\"up-to-date\",\"message\":\"You're up to date!. 7.3.32 is currently the newest version available.\"}"
-}
