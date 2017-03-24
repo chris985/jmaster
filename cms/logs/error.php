95ebc4eb2cff32246cb01dc48aaa68c815b24f48
Chris98.5 <cstarcrescendo@gmail.com>
2017-03-24 12:16:17 -0400

    Now you dont get my configuration files anymore you stupid hacker. i see u

---
+++ cms/logs/error.php
@@ -0,0 +1,12 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-06-19 15:50:08 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-06-19T15:50:08+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-10-29T19:36:32+00:00	INFO 192.168.6.176	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-10-29T19:36:45+00:00	INFO 192.168.6.176	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-10-29T19:37:13+00:00	INFO 192.168.6.176	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-10-29T19:56:38+00:00	INFO 192.168.6.176	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-06-04T20:10:43+00:00	INFO 104.228.163.17	joomlafailure	Empty password not allowed.
---
+++ cms/logs/joomla_update.php
@@ -0,0 +1,61 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-05-01 16:12:10 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-05-01T16:12:10+00:00	INFO	update	Update started by user Christopher Martone (523). Old version is 3.2.3.
+2014-05-01T16:12:10+00:00	INFO	update	Downloading update file from .
+2014-05-01T16:12:14+00:00	INFO	update	File Joomla_3.3.0-Stable-Update_Package.zip successfully downloaded.
+2014-05-01T16:12:14+00:00	INFO	update	Starting installation of new version.
+2014-05-01T16:12:20+00:00	INFO	update	Finalising installation.
+2014-05-01T16:12:20+00:00	INFO	update	Ran query from file 3.3.0-2014-02-16. Query text: ALTER TABLE `#__users` ADD COLUMN `requireReset` tinyint(4) NOT NULL DEFAULT 0 C.
+2014-05-01T16:12:20+00:00	INFO	update	Ran query from file 3.3.0-2014-04-02. Query text: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2014-05-01T16:12:20+00:00	INFO	update	Deleting removed files and folders.
+2014-05-01T16:12:35+00:00	INFO	update	Cleaning up after installation.
+2014-05-01T16:12:35+00:00	INFO	update	Update to version 3.3.0 is complete.
+2014-06-20T15:56:10+00:00	INFO	update	Update started by user Christopher Martone (523). Old version is 3.3.0.
+2014-06-20T15:56:11+00:00	INFO	update	Downloading update file from .
+2014-06-20T15:56:12+00:00	INFO	update	File Joomla_3.3.x_to_3.3.1-Stable-Patch_Package.zip successfully downloaded.
+2014-06-20T15:56:12+00:00	INFO	update	Starting installation of new version.
+2014-06-20T15:56:16+00:00	INFO	update	Finalising installation.
+2014-06-20T15:56:16+00:00	INFO	update	Deleting removed files and folders.
+2014-06-20T15:56:30+00:00	INFO	update	Cleaning up after installation.
+2014-06-20T15:56:30+00:00	INFO	update	Update to version 3.3.1 is complete.
+2014-10-29T19:08:57+00:00	INFO	update	Update started by user Christopher Martone (523). Old version is 3.3.1.
+2014-10-29T19:08:57+00:00	INFO	update	Downloading update file from .
+2014-10-29T19:08:59+00:00	INFO	update	File Joomla_3.3.x_to_3.3.6-Stable-Patch_Package.zip successfully downloaded.
+2014-10-29T19:08:59+00:00	INFO	update	Starting installation of new version.
+2014-10-29T19:09:05+00:00	INFO 192.168.6.176	update	Finalising installation.
+2014-10-29T19:09:05+00:00	INFO 192.168.6.176	update	Ran query from file 3.3.4-2014-08-03. Query text: ALTER TABLE `#__user_profiles` CHANGE `profile_value` `profile_value` TEXT NOT N.
+2014-10-29T19:09:05+00:00	INFO 192.168.6.176	update	Ran query from file 3.3.6-2014-09-30. Query text: INSERT INTO `#__update_sites` (`name`, `type`, `location`, `enabled`) VALUES ('J.
+2014-10-29T19:09:05+00:00	INFO 192.168.6.176	update	Ran query from file 3.3.6-2014-09-30. Query text: INSERT INTO `#__update_sites_extensions` (`update_site_id`, `extension_id`) VALU.
+2014-10-29T19:09:06+00:00	INFO 192.168.6.176	update	Deleting removed files and folders.
+2014-10-29T19:09:25+00:00	INFO 192.168.6.176	update	Cleaning up after installation.
+2014-10-29T19:09:25+00:00	INFO 192.168.6.176	update	Update to version 3.3.6 is complete.
+2015-03-30T14:54:02+00:00	INFO 192.168.6.176	update	Update started by user Christopher Martone (523). Old version is 3.3.6.
+2015-03-30T14:54:02+00:00	INFO 192.168.6.176	update	Downloading update file from https://github.com/joomla/joomla-cms/releases/download/3.4.1/Joomla_3.4.1-Stable-Update_Package.zip.
+2015-03-30T14:54:06+00:00	INFO 192.168.6.176	update	File Joomla_3.4.1-Stable-Update_Package.zip successfully downloaded.
+2015-03-30T14:54:06+00:00	INFO 192.168.6.176	update	Starting installation of new version.
+2015-03-30T14:54:27+00:00	INFO 192.168.6.176	update	Finalising installation.
+2015-03-30T14:54:27+00:00	INFO 192.168.6.176	update	Ran query from file 3.4.0-2014-08-24. Query text: INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description.
+2015-03-30T14:54:27+00:00	INFO 192.168.6.176	update	Ran query from file 3.4.0-2014-09-01. Query text: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2015-03-30T14:54:28+00:00	INFO 192.168.6.176	update	Ran query from file 3.4.0-2014-09-01. Query text: INSERT INTO `#__update_sites` (`name`, `type`, `location`, `enabled`) VALUES ('W.
+2015-03-30T14:54:28+00:00	INFO 192.168.6.176	update	Ran query from file 3.4.0-2014-09-01. Query text: INSERT INTO `#__update_sites_extensions` (`update_site_id`, `extension_id`) VALU.
+2015-03-30T14:54:29+00:00	INFO 192.168.6.176	update	Ran query from file 3.4.0-2014-09-16. Query text: ALTER TABLE `#__redirect_links` ADD header smallint(3) NOT NULL DEFAULT 301;.
+2015-03-30T14:54:30+00:00	INFO 192.168.6.176	update	Ran query from file 3.4.0-2014-09-16. Query text: ALTER TABLE `#__redirect_links` MODIFY new_url varchar(255);.
+2015-03-30T14:54:30+00:00	INFO 192.168.6.176	update	Ran query from file 3.4.0-2014-10-20. Query text: DELETE FROM `#__extensions` WHERE `extension_id` = 100;.
+2015-03-30T14:54:30+00:00	INFO 192.168.6.176	update	Ran query from file 3.4.0-2014-12-03. Query text: UPDATE `#__extensions` SET `protected` = '0' WHERE `name` = 'plg_editors-xtd_art.
+2015-03-30T14:54:31+00:00	INFO 192.168.6.176	update	Ran query from file 3.4.0-2015-01-21. Query text: INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description.
+2015-03-30T14:54:31+00:00	INFO 192.168.6.176	update	Ran query from file 3.4.0-2015-02-26. Query text: INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description.
+2015-03-30T14:54:31+00:00	INFO 192.168.6.176	update	Deleting removed files and folders.
+2015-03-30T14:55:02+00:00	INFO 192.168.6.176	update	Cleaning up after installation.
+2015-03-30T14:55:02+00:00	INFO 192.168.6.176	update	Update to version 3.4.1 is complete.
+2016-03-14T14:03:51+00:00	INFO 173.64.9.2	update	Update started by user Christopher Martone (523). Old version is 3.4.1.
+2016-03-14T14:03:51+00:00	INFO 173.64.9.2	update	Downloading update file from https://github.com/joomla/joomla-cms/releases/download/3.4.8/Joomla_3.4.x_to_3.4.8-Stable-Patch_Package.zip.
+2016-03-14T14:03:51+00:00	INFO 173.64.9.2	update	File Joomla_3.4.x_to_3.4.8-Stable-Patch_Package.zip successfully downloaded.
+2016-03-14T14:03:52+00:00	INFO 173.64.9.2	update	Starting installation of new version.
+2016-03-14T14:03:58+00:00	INFO 173.64.9.2	update	Finalising installation.
+2016-03-14T14:03:58+00:00	INFO 173.64.9.2	update	Deleting removed files and folders.
+2016-03-14T14:14:57+00:00	INFO 173.64.9.2	update	Cleaning up after installation.
+2016-03-14T14:14:57+00:00	INFO 173.64.9.2	update	Update to version 3.4.8 is complete.
